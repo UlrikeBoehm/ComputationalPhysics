31b361fcc297e6efe5d97e8abfc3741fb458fa48
Ulrike Boehm <ulrike.boehm@gmail.com>
2019-01-25 20:40:34 -0500

    additional python files

--- 02_PythonProgrammingForPhysistics/polar.py
+++ 02_PythonProgrammingForPhysistics/polar.py
@@ -1,10 +1,15 @@
 from math import sin, cos, pi
 
+# Ask the user foor the values of the radius and angle
 r = float(input("Enter r: "))
 d = float(input("Enter theta in degrees: "))
 
+# Convert the angle to radius
 theta = d*pi/180
+
+# Calculate the equivalent Cartesian coordinates
 x = r*cos(theta)
 y = r*sin(theta)
 
+# Print out the results
 print("x =" ,x, "y =" ,y)
